fac86398a406412431494092f135543f18508c93
rchiu5 <32538202+rchiu5@users.noreply.github.com>
2017-11-18 20:05:44 -0600

    Add setupDate()

--- Sender/Sender.ino
+++ Sender/Sender.ino
@@ -12,7 +12,7 @@ const int rs = 12, en = 11, d4 = 7, d5 = 6, d6 = 5, d7 = 4;
 LiquidCrystal lcd (rs, en, d4, d5, d6, d7);
 
 // set up the bluetooth serial port:
-const int rx = 2, tx = 3;
+const int rx = 3, tx = 2;
 SoftwareSerial BTSerial(tx, rx);
 
 // set up the infrared receiver:
@@ -25,7 +25,7 @@ const int red = 13, green = 1, blue = 0;
 RGBMood mood(red, green, blue);
 
 // set up the notes in the melody and its duration
-notes in the melody:
+// notes in the melody:
 int melody[] = {NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4};
 int noteDurations[] = {4, 8, 8, 4, 4, 4, 4, 4};
 int nNotes = 8;
@@ -41,9 +41,9 @@ int mo = 1, dy = 1, yr = 0, hr = 0, mi = 0, sc = 0;
 int val1, val2;
 int TS_state = LOW;
 int wait_mode = LOW;
-char fed_signal;
 int numFeedings = 0;
 int LED_state = HIGH;
+int LIMIT = 3;
 
 
 void setup() {
@@ -57,9 +57,10 @@ void setup() {
   // set up serial library at 9600 bps:
   Serial.begin(9600);
 
-  // set up bluetooth serial library at 38400 bps:
-  BTSerial.begin(38400);
+  // set up bluetooth serial library at 9600 bps:
+  BTSerial.begin(9600);
 
+  // enable IR receiver:
   irrecv.enableIRIn();
 
   // set up the LED mood settings
@@ -68,6 +69,10 @@ void setup() {
   mood.setFadingSpeed(25);
   mood.setHoldingTime(0);
   mood.fadeHSB(0, 255, 255);
+
+  // set up date via IR receiver input:
+  setupDate();
+  updateDateAndTime();
 }
 
 void loop() {
@@ -76,68 +81,66 @@ void loop() {
   }
   
   if(irrecv.decode(&results)) {
-    if(translateIR() == 'P') {
+    char IRkey = translateIR();
+    
+    if(IRkey == 'P') {
       // turn LED on/off
       LED_state = !LED_state;
     }
-    else if(translateIR() == '-') {
+    else if(IRkey == '-') {
       // turn off music
-      noTone(8);
+      noTone(buzzer);
     }
+
+    // receive the next value:
+    irrecv.resume();
   }
   
   if(wait_mode == HIGH) {
     // to receive signal from other Arduino
     while(BTSerial.available() > 0) {
-      if(BTSerial.read() == "time") {
+      char BTkey = BTSerial.read();
+      
+      if(BTkey == 'T') {
         // get input of date and time from other Arduino:
-        mo_in = BTSerial.parseInt();
-        dy_in = BTSerial.parseInt();
-        yr_in = BTSerial.parseInt();
-        hr_in = BTSerial.parseInt();
-        mi_in = BTSerial.parseInt();
-        sc_in = BTSerial.parseInt();
-  
+        hr = BTSerial.read();
+        mi = BTSerial.read();
+        sc = BTSerial.read();
+        
         // update the date and time:
         setTime(hr, mi, sc, dy, mo, yr);
       }
-      else {
-        fed_signal = BTSerial.read();
-        
-        if(fed_signal == '1')
-        {
-          numFeedings++;
-          wait_mode = LOW;
-          break;
-        }
+      else if(BTkey == 'F') {
+        numFeedings++;
+        wait_mode = LOW;
       }
     }
   }
   else {
     // to send signal to receiving Arduino
-    val1 = digitalRead(tsensor);      // read input value and store into val1
+    val1 = digitalRead(tsensor);        // read input value and store into val1
     delay(10);
-    val2 = digitalRead(tsensor);      // read input value again to check for bounces
+    val2 = digitalRead(tsensor);        // read input value again to check for bounces
   
-    if(val1 == val2) {                // make sure the 2 readings are consistent
-      if(val1 != TS_state) {          // check if state has changed
-        if(val1 == HIGH) {            // check if touch sensor was triggered
+    if(val1 == val2) {                  // make sure the 2 readings are consistent
+      if(val1 != TS_state) {            // check if state has changed
+        if(val1 == HIGH) {              // check if touch sensor was triggered
           
           if(numFeedings < LIMIT) {
-            Serial.write("Feed Me");  // send message
-            BTSerial.write("1");      // send a signal
+            Serial.println("Feed Me");  // send message
+            BTSerial.write("1");        // send a signal
             wait_mode = HIGH;
-
-            // play background music:
+            
+            // play a short melody:
             for(int i = 0; i < nNotes; i++) {
               int noteDuration = 1000 / noteDurations[i];
-              tone(8, melody[i], noteDuration);
+              tone(buzzer, melody[i], noteDuration);
 
               delay(noteDuration * 1.30);
-              noTone(8);
+              noTone(buzzer);
             }
           }
-
+          
           // new day, restart count to activate touch sensor:
           if(hour() == 0 && minute() == 0 && second() == 0)
           {
@@ -151,7 +154,61 @@ void loop() {
   }
   
   updateDateAndTime();
-  delay(1000);
+}
+
+void setupDate() {
+  int count = 0;
+  int digit = -1;
+  
+  while(true) {
+    char rec = 'Z';
+
+    if(irrecv.decode(&results)) {
+      rec = translateIR();
+      digit = isDigit(rec);
+      irrecv.resume();
+      
+      if(count == 0) {  // month
+        if(rec == '0' || digit == 1) {
+          mo = digit * 10;
+          count++;
+        }
+      }
+      else if(count == 1) {
+        if((mo == 0 && digit > 0 && digit < 10) ||
+           (mo == 10 && (rec == '0' || digit == 1 || digit == 2))) {
+          mo += digit;
+          count++;
+        }
+      }
+      else if(count == 2) {  // day
+        if(rec == '0' || (digit > 0 && digit < 4)) {
+          dy = digit * 10;
+          count++;
+        }
+      }
+      else if(count == 3) {
+        if((dy == 0 && digit > 0 && digit < 10) ||
+           (dy == 30 && (rec == '0' || digit == 1)) ||
+           (rec == '0' || (digit > 0 && digit < 10))) {
+          dy += digit;
+          count++;
+        }
+      }
+      else if(count == 4) {  // year
+        if(rec == '0' || (digit > 0 && digit < 10)) {
+          yr = digit * 10;
+          count++;
+        }
+      }
+      else if(count == 5) {
+        if(rec == '0' || (digit > 0 && digit < 10)) {
+          yr += digit;
+          break;
+        }
+      }
+    }
+  }
 }
 
 void updateDateAndTime() {
